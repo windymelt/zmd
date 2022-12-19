9f5e19e5b0607a0c639193a28e69c81ccb6dbc0f
windymelt <windymelt@3qe.us>
2022-12-19 23:49:27 +0900

    oops

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,7 +77,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/windymelt/zmm',
             label: 'GitHub',
             position: 'right',
           },
